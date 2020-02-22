644af7af6f19a9077379388f73c98de30bb0fe32
EntilZha <ski.rodriguez@gmail.com>
2020-02-22 09:04:48 -0500

    updates

--- src/qb/evaluate.py
+++ src/qb/evaluate.py
@@ -1,5 +1,11 @@
+from typing import Text
+import os
 import tqdm
+import comet_ml
 import numpy as np
+from allennlp.models.archival import load_archive
+from qb.predictor import QbPredictor
+from qb.data import QantaReader, QANTA_GUESSDEV
 
 
 def compute_accuracy(predictor, instances):
@@ -23,3 +29,33 @@ def compute_accuracy(predictor, instances):
         correct.append(example['metadata']['page'] == predicted_page)
         pred_pages.append(predicted_page)
     return np.mean(correct)
+
+
+def score_model(serialization_dir: Text, log_to_comet=False):
+    archive = load_archive(
+        os.path.join(serialization_dir, 'model.tar.gz'),
+        cuda_device=0
+    )
+    predictor = QbPredictor.from_archive(
+        archive,
+        predictor_name='qb.predictor.QbPredictor',
+    )
+    dev_first_sentence = QantaReader(
+        'guessdev',
+        break_questions=False,
+        first_sentence_only=True,
+        include_label=False,
+    ).read(QANTA_GUESSDEV)
+    dev_full_question = QantaReader(
+        'guessdev',
+        break_questions=False,
+        include_label=False,
+    ).read(QANTA_GUESSDEV)
+    accuracy_start_dev = compute_accuracy(predictor, dev_first_sentence)
+    accuracy_full_dev = compute_accuracy(predictor, dev_full_question)
+    print('first', 'dev', accuracy_start_dev)
+    print('full', 'dev', accuracy_full_dev)
+    if log_to_comet:
+        experiment = comet_ml.get_global_experiment()
+        experiment.log_metric(f'dev_first_accuracy', accuracy_start_dev)
+        experiment.log_metric(f'dev_full_accuracy', accuracy_full_dev)
--- src/qb/main.py
+++ src/qb/main.py
@@ -1,22 +1,14 @@
-import os
 import subprocess
-import glob
 
-import numpy as np
 import click
 import toml
 import comet_ml
-import tqdm
 from allennlp.commands import train
-from allennlp.models.archival import load_archive
 
 from qb.util import get_logger
 from qb.config import Config
 from qb.hyper import hyper_cli
-from qb.predictor import QbPredictor
-from qb.model import Guesser
-from qb.data import QantaReader, QANTA_GUESSDEV, QANTA_TEST
-from qb.evaluate import compute_accuracy
+from qb.evaluate import compute_accuracy, score_model
 # imports for allennlp register
 from qb import model
 from qb import callbacks
@@ -53,6 +45,7 @@ def cli_train(config_path):
         file_friendly_logging=True,
         force=True,
     )
+    score_model(conf['serialization_dir'], log_to_comet=True)
     subprocess.run(f'touch {conf["serialization_dir"]}/COMPLETE', shell=True, check=True)
     subprocess.run(f'fusermount -u /tmp/{generated_id}', shell=True, check=True)
     subprocess.run(f'rm -rf /tmp/{generated_id}')
@@ -64,40 +57,7 @@ def cli_evaluate(config_path):
     with open(config_path) as f:
         conf = toml.load(f)
     serialization_dir = conf['serialization_dir']
-    archive = load_archive(
-        os.path.join(serialization_dir, 'model.tar.gz'),
-        cuda_device=0
-    )
-    predictor = QbPredictor.from_archive(
-        archive,
-        predictor_name='qb.predictor.QbPredictor',
-    )
-    dev_first_sentence = QantaReader(
-        'guessdev',
-        break_questions=False,
-        first_sentence_only=True,
-        include_label=False,
-    ).read(QANTA_GUESSDEV)
-    dev_full_question = QantaReader(
-        'guessdev',
-        break_questions=False,
-        include_label=False,
-    ).read(QANTA_GUESSDEV)
-    test_first_sentence = QantaReader(
-        'guesstest',
-        break_questions=False,
-        first_sentence_only=True,
-        include_label=False,
-    ).read(QANTA_TEST)
-    test_full_question = QantaReader(
-        'guesstest',
-        break_questions=False,
-        include_label=False,
-    ).read(QANTA_TEST)
-    print('first', 'dev', compute_accuracy(predictor, dev_first_sentence))
-    print('full', 'dev', compute_accuracy(predictor, dev_full_question))
-    print('first', 'test', compute_accuracy(predictor, test_first_sentence))
-    print('full', 'test', compute_accuracy(predictor, test_full_question))
+    score_model(serialization_dir)
 
 
 if __name__ == '__main__':
